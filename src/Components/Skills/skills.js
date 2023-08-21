50aca1cf34de1c6905b411d6bed29bf0290113bd
Tricia Sawyer <tricia.sawyer03@gmail>
2023-08-21 14:05:07 -0500

    try to add profile pic

--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -3,18 +3,28 @@ import { DiJavascript1, DiReact, DiCss3, DiHtml5, DiMongodb, DiNodejs } from 're
 import './skills.css';
 
 const Skills = () => {
+  const skillsData = [
+    { icon: <DiHtml5 className="skill-icon" />, name: 'HTML5' },
+    { icon: <DiCss3 className="skill-icon" />, name: 'CSS3' },
+    { icon: <DiJavascript1 className="skill-icon" />, name: 'JavaScript' },
+    { icon: <DiReact className="skill-icon" />, name: 'React' },
+    { icon: <DiNodejs className="skill-icon" />, name: 'Node.js' },
+    { icon: <DiMongodb className="skill-icon" />, name: 'MongoDB' },
+  ];
+
   return (
     <section className="skills-section">
       <div className="container">
         <h1 className="section-title">Skills</h1>
         <div className="skills-icons">
-          <DiHtml5 className="skill-icon" />
-          <DiCss3 className="skill-icon" />
-          <DiJavascript1 className="skill-icon" />
-          <DiReact className="skill-icon" />
-          <DiNodejs className="skill-icon" />
-          <DiMongodb className="skill-icon" />
-
+          {skillsData.map((skill, index) => (
+            <div key={index} className="skill">
+              <div className="icon-container">
+                {skill.icon}
+                <span className="skill-name">{skill.name}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
