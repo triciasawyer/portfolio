abde5ba8d252647c3a5069d05d767ce7ea6bc744
Tricia Sawyer <tricia.sawyer03@gmail>
2023-08-30 14:43:48 -0500

    add project 1 images

--- src/Components/Content/content.js
+++ src/Components/Content/content.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Project from '../Projects/projects';
 import profileImg from '../../images/imgp.png';
+import capsDriver from '../../assets/caps-driver.png';
+import capsFlowerV from '../../assets/caps-flowerV.png';
+import capsWidgetV from '../../assets/caps-widgetV.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './content.css';
 
@@ -11,14 +14,14 @@ const Content = () => {
 
   const projects = [
     {
-      title: "Project 1",
-      date: "July 2022",
-      languages: "JavaScript, React",
+      title: "Core administrative processing systems (CAPS system)",
+      date: "May 2022",
+      languages: "JavaScript, Node.js",
       details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       images: [
-        'https://via.placeholder.com/500x400',
-        'https://via.placeholder.com/500x400',
-        'https://via.placeholder.com/500x400',
+        capsDriver,
+        capsFlowerV,
+        capsWidgetV,
       ],
     },
     {
