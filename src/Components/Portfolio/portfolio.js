e21c42e3591fa2c53d7a9912bf3896117d877481
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-12 15:47:19 -0500

    add cards with placeholders for portfolio route

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -1,14 +1,47 @@
 import React from 'react';
-// import './portfolio.css';
+import Project from '../Projects/projects';
+import './portfolio.css';
 
 const Portfolio = () => {
+  const projects = [
+    {
+      title: "Project 1",
+      date: "May 2023",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 2",
+      date: "July 2023",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 3",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 4",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    // Add more projects later
+  ];
+
   return (
-    <>
-      <div className='portfolio-container'>
-        <h1>portfolio.</h1>
+    <div className='portfolio-container'>
+      <h1>portfolio.</h1>
+      <div className="project-grid">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            date={project.date}
+            images={project.images}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Portfolio;
