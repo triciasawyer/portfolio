ccf62229cc83c206d9aee1b992d2cb795e8309e6
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-12 15:31:37 -0500

    edit about content

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -18,13 +18,13 @@ const About = () => {
         </div>
         <div className="about-text">
           <p>
-            Hello! I'm Tricia Sawyer, a passionate Front-End Developer based in Cedar Rapids, Iowa.
+            Hello! I'm Tricia Sawyer, a passionate full-stack Developer based in Cedar Rapids, Iowa.
           </p>
           <p>
-            I have a strong passion for creating visually appealing and user-friendly web experiences. With a background in teaching, I bring [Your Unique Skills] to the table.
+            With a background in teaching, I bring [Your Unique Skills] to the table.
           </p>
           <p>
-            When I'm not coding, you can find me on the lake kyaking, or just enjoying the outdoors being active.
+            When I'm not coding, you can find me out on the lake, or simply enjoying the outdoors.
           </p>
           <Skills />
         </div>
