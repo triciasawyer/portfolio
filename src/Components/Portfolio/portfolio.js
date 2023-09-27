5b3a073df170d3f1bc1db04e03626b4d9fa82c92
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-27 10:42:40 -0500

    add more placeholders for projects and style them

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -24,7 +24,47 @@ const Portfolio = () => {
       date: "September 2022",
       images: ["https://via.placeholder.com/500x400"],
     },
-    // Add more projects later
+    {
+      title: "Project 5",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 6",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 7",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 8",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 9",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 10",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 11",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    {
+      title: "Project 12",
+      date: "September 2022",
+      images: ["https://via.placeholder.com/500x400"],
+    },
+    
   ];
 
   return (
