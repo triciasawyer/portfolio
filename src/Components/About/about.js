fa124dbb510086e326a96a1879d1b658c247ceae
Tricia Sawyer <tricia.sawyer03@gmail>
2023-09-29 15:27:49 -0500

    edit styling and add image

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Skills from '../Skills/skills';
-// import profImg from '../../images/aboutProf.png';
+import profImg from '../../images/imgp.png';
 import './about.css';
 
 const About = () => {
@@ -8,15 +8,15 @@ const About = () => {
     <>
       <div className="about-con">
         <Skills />
+        <div className="about-img">
         <div className="header-p">
           <h1 className="about-h1">about</h1>
-          <div className="about-text">
-            <p>I'm a full-stack Software Developer based in Cedar Rapids, Iowa.</p>
+            <p className="about-text1">I'm a full-stack Software Developer based in Cedar Rapids, Iowa.</p>
             {/* <p> With a background in teaching, I bring [Your Unique Skills] to the table.</p> */}
-            <p>When I'm not creating digital magic, you'll find me out on the lake or watching the newest movies.</p>
-          </div>
+            <p className="about-text2">When I'm not creating digital magic, you'll find me out on the lake, cheering on my favorite sports teams, or watching the latest movies.</p>
         </div>
-        {/* <img src={profImg} alt="Tricia Sawyer" className="prof-img" /> */}
+        <img src={profImg} alt="Tricia Sawyer" className="prof-img" />
+        </div>
       </div>
     </>
   );
