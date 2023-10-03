f5c516872e43c7c083427471c45acc2a671acbad
Tricia Sawyer <tricia.sawyer03@gmail>
2023-10-02 22:57:23 -0500

    add project and link

--- src/Components/Portfolio/portfolio.js
+++ src/Components/Portfolio/portfolio.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Resty from '../../images/resty.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Resty from '../../assets/resty.png';
+import Algorithms from '../../assets/datastructures.png'
+import AdventureTime from '../../assets/adventure-time-fe.png';
 import './portfolio.css';
 
 const Portfolio = () => {
   const projects = [
     {
       title: "Resty",
-      date: "May 2023",
+      date: "June 2023",
+      link: "https://github.com/triciasawyer/resty",
       images: Resty,
     },
     {
@@ -25,9 +30,10 @@ const Portfolio = () => {
       images: ["https://via.placeholder.com/500x400"],
     },
     {
-      title: "Project 5",
-      date: "September 2022",
-      images: ["https://via.placeholder.com/500x400"],
+      title: "Data Structures and Algorithm",
+      date: "May 2023",
+      link: "https://github.com/triciasawyer/data-structures-and-algorithms/tree/main/javascript/401-code-challenges",
+      images: Algorithms,
     },
     {
       title: "Project 6",
@@ -45,9 +51,10 @@ const Portfolio = () => {
       images: ["https://via.placeholder.com/500x400"],
     },
     {
-      title: "Project 9",
-      date: "September 2022",
-      images: ["https://via.placeholder.com/500x400"],
+      title: "Adventure time",
+      date: "July 2023",
+      link: "https://github.com/orgs/final-project-401/repositories",
+      images: AdventureTime,
     },
     {
       title: "Project 10",
@@ -76,6 +83,11 @@ const Portfolio = () => {
             <img src={project.images} alt={project.title} />
             <h2 className="card-title">{project.title}</h2>
             <p className="card-date">{project.date}</p>
+            {/* <div className="click-indicator"> */}
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faArrowRight} className='arrow-icon' />
+            </a>
+            {/* </div> */}
           </div>
         ))}
       </div>
